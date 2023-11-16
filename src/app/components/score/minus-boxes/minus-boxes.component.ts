d7a171a007c82cfaefe55567705d2e083ef4b486
jdvoeller <jordanvoeller@gmail.com>
2023-11-16 14:03:57 -0700

    minus boxes finished

--- src/app/components/score/minus-boxes/minus-boxes.component.ts
+++ src/app/components/score/minus-boxes/minus-boxes.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 
-interface MinusData {
+export interface MinusData {
 	boxNum: number,
 	selected: boolean,
 }
@@ -11,6 +11,7 @@ interface MinusData {
 })
 
 export class MinusBoxesComponent implements OnInit {
+	@Output() boxClickedEvent: EventEmitter<MinusData[]> = new EventEmitter();
 	public boxes: MinusData[];
 	constructor() {
 		this.boxes = [
@@ -31,6 +32,12 @@ export class MinusBoxesComponent implements OnInit {
 				selected: false,
 			},
 		]
+	}
+
+	boxClicked(boxNum: number) {
+		const BOX = this.boxes.filter((b) => b.boxNum === boxNum)[0];
+		BOX.selected = !BOX.selected;
+		this.boxClickedEvent.emit(this.boxes);
 	}
 
 	ngOnInit() { }
--- src/app/components/score/score.component.ts
+++ src/app/components/score/score.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { MinusData } from './minus-boxes/minus-boxes.component';
 
 @Component({
 	selector: 'score',
@@ -8,7 +9,19 @@ import { Component, Input, OnInit } from '@angular/core';
 
 export class ScoreComponent implements OnInit {
 	@Input() score!: number;
+
+	public minusPoints: number = 0;
 	constructor() { }
 
 	ngOnInit() { }
+
+	minuBoxClicked(boxes: MinusData[]) {
+		let total = 0;
+		boxes.forEach((box) => {
+			if (box.selected) {
+				total -= 5;
+			}
+		});
+		this.minusPoints = total;
+	}
 }
